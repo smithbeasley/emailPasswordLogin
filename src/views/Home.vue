f829d5ec9c6d90798ea57c8b7a3b8b54cc7b0f84
smithbeasley@gmail.com <smithbeasley@gmail.com>
2021-04-21 21:03:58 -0500

    emailPasswordLogin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,10 @@
-<template>Home Page</template>
+<template>
+<div class="mt-8">
+    <p class="text-2xl text-center mx-auto m-8 tracking-wider">Welcome {{user?.email || 'New User'}} </p>
+ <img src="../assets/home.svg" alt="Home" class="h-40 mx-auto mt-40"> 
+</div>
+</template>
+
+<script setup>
+import {user} from '../helpers/useAuth'
+</script>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,5 @@
 <template>
-    404 - Page not Found
+   <div>
+ <img src="../assets/404.svg" alt="404 Not Found" class="h-40 mx-auto mt-40"> 
+   </div>
 </template>
